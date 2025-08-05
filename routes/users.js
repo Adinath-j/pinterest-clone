58da46dcdf19c4b4634954c63db793652267b568
Adinath Jadhav <adinath0632@gmail.com>
2025-08-05 19:24:28 +0530

    updated code and fixed Mongodb Integration

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,13 @@
 const mongoose = require("mongoose")
 const passport = require("passport")
 const plm = require('passport-local-mongoose')
-mongoose.connect("mongodb://127.0.0.1:27017/pinterest")
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("✅ Connected to MongoDB"))
+.catch(err => console.error("❌ MongoDB connection error:", err));
+
 
 const userSchema = mongoose.Schema({
   username: String,
