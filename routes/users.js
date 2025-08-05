f8c4917c94cda8d6dce799b2e73d312db640327c
Adinath Jadhav <adinath0632@gmail.com>
2025-08-05 19:35:23 +0530

    modified users.js

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,8 @@
 const mongoose = require("mongoose")
 const passport = require("passport")
 const plm = require('passport-local-mongoose')
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log("✅ Connected to MongoDB"))
-.catch(err => console.error("❌ MongoDB connection error:", err));
+mongoose.connect(process.env.MONGODB_URI)
+
 
 
 const userSchema = mongoose.Schema({
